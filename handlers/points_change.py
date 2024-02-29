af7fa4a77c6980bb8493503e68b0ab9356bf39de
mranw <andrey-birula@mail.ru>
2024-02-29 04:19:05 +0300

    Create file for data base (still empty).

--- handlers/points_change.py
+++ handlers/points_change.py
@@ -3,7 +3,6 @@ from typing import List
 from aiogram import Router, F
 from aiogram.types import Message
 
-
 from filters.chat_type import ChatTypeFilter
 from filters.find_usernames import HasUsernamesFilter
 
@@ -21,15 +20,37 @@ async def plus_point_message_with_usernames(
         message: Message,
         usernames: List[str]
 ):
-    for user in message.text:
-        if '+1 балл' in message.text:
-            await message.reply(f"Пользователю @{user.full_name} начислен +1 балл")
-
-    if '+2 балла' in message.text:
-        await message.reply(f"Пользователю начислено +2 балла")
+    if '+1 балл' in message.text:
+        if len(usernames) == 1:
+            await message.reply(f"Пользователю {", ".join(usernames)} начислен +1 балл")
+        elif len(usernames) > 1:
+            await message.reply(f"Пользователям {", ".join(usernames)} начислен +1 балл")
+    elif '+2 балла' in message.text:
+        if len(usernames) == 1:
+            await message.reply(f"Пользователю {", ".join(usernames)} начислено +2 балла")
+        elif len(usernames) > 1:
+            await message.reply(f"Пользователям {", ".join(usernames)} начислено +2 балла")
     elif '+3 балла' in message.text:
-        await message.answer(f"Пользователю {message.reply} начислено +3 балла")
+        if len(usernames) == 1:
+            await message.reply(f"Пользователю {", ".join(usernames)} начислено +3 балла")
+        elif len(usernames) > 1:
+            await message.reply(f"Пользователям {", ".join(usernames)} начислено +3 балла")
     elif '+4 балла' in message.text:
-        await message.answer("Пользователю начислено +4 балла")
-    elif '+5 балла' in message.text:
-        await message.answer("Пользователю начислено +5 баллов")
+        if len(usernames) == 1:
+            await message.reply(f"Пользователю {", ".join(usernames)} начислено +4 балла")
+        elif len(usernames) > 1:
+            await message.reply(f"Пользователям {", ".join(usernames)} начислено +4 балла")
+    elif '+5 баллов' in message.text:
+        if len(usernames) == 1:
+            await message.reply(f"Пользователю {", ".join(usernames)} начислено +5 баллов")
+        elif len(usernames) > 1:
+            await message.reply(f"Пользователям {", ".join(usernames)} начислено +5 баллов")
+    else:
+        if len(usernames) == 1:
+            await message.reply("Неверный формат сообщения о начислении баллов.\n\n"
+                                f"Пользователю {", ".join(usernames)} не были начислены баллы."
+                                "Вы можете начислить до пяти баллов за раз")
+        elif len(usernames) > 1:
+            await message.reply("Неверный формат сообщения о начислении баллов.\n\n"
+                                f"Пользователям {", ".join(usernames)} не были начислены баллы."
+                                "Вы можете начислить до пяти баллов за раз")
